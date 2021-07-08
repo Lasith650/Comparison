198b2b529b9c282545ee3d628a0411b173b1f4c9
lasith650 <lasith650@gmail.com>
2021-07-09 01:28:45 +0530

    final

--- src/ComparisonHandler.java
+++ src/ComparisonHandler.java
@@ -1,20 +1,35 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.wso2.choreo.analytics.rest.api.conf.APISecurityConfig;
+import org.wso2.choreo.analytics.rest.api.exceptions.BadTokenException;
+import org.wso2.choreo.analytics.rest.api.exceptions.TokenGenerationException;
+import org.wso2.choreo.analytics.rest.api.logging.LoggingData;
+import org.wso2.choreo.analytics.rest.api.logging.LoggingDataContextHolder;
+import reactor.core.publisher.Mono;
 
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
 
 public class ComparisonHandler {
     public boolean compare(ArrayList<String> associated_strides_for_cwe,
